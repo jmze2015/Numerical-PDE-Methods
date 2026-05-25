5132e9840cbad71c22cc1eede49fa2ff6a7798b9
Jose Munoz-Lopez <jmze2015@mymail.pomona.edu>
2026-05-25 14:17:08 -0700

    adding Godunov screenshots

--- code/burgers-Godunov.R
+++ code/burgers-Godunov.R
@@ -135,6 +135,70 @@ fig
 
 htmlwidgets::saveWidget(fig, "burgers_godunov_animation.html")
 
+## Screenshot Code
+times <- c(0, 0.24, 0.80, 1.28, 2.40)
+
+dir.create("Images_Godunov", showWarnings = FALSE)
+
+for (tt in times) {
+  
+  df_t <- results[results$time == tt, ]
+  
+  fig_t <- plot_ly(
+    data = df_t,
+    x = ~x,
+    y = ~u,
+    type = "scatter",
+    mode = "lines"
+  ) %>%
+    layout(
+      title = paste0("Inviscid Burgers': Godunov, t = ", tt),
+      xaxis = list(title = "x", range = c(-2.6, 2.6)),
+      yaxis = list(title = "u(x,t)", range = c(0, 1.1)),
+      shapes = list(
+        list(
+          type = "line",
+          x0 = L/2,
+          x1 = L/2,
+          y0 = 0,
+          y1 = 2.2,
+          line = list(color = "blue", width = 2, dash = "dash")
+        ),
+        list(
+          type = "line",
+          x0 = -L/2,
+          x1 = -L/2,
+          y0 = 0,
+          y1 = 2.2,
+          line = list(color = "orange", width = 2, dash = "dash")
+        )
+      )
+    )
+  
+  filename <- paste0(
+    "Images_Godunov/Godunov_t_",
+    gsub("\\.", "p", sprintf("%.2f", tt)),
+    ".png"
+  )
+  
+  htmlwidgets::saveWidget(
+    fig_t,
+    "temp.html",
+    selfcontained = TRUE
+  )
+  
+  webshot2::webshot(
+    "temp.html",
+    file = filename,
+    vwidth = 800,
+    vheight = 600
+  )
+  
+}
+
+
+
+
 
 
 
